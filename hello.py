5171e364c1deac89ea176cf50c117a75353eaf01
pilar <pilarruete@gmail.com>
2019-02-04 19:55:09 +0100

    He añadido otro saludo

--- hello.py
+++ hello.py
@@ -1,2 +1,3 @@
 def main():
 	print 'Hola mundo!'
+	print 'Estoy modificando el repositorio Ejemplo que me he clonado para practicar con git. Ruego disculpéis las molestias y espero no fastidiarla.'
