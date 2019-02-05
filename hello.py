174accb8489b8fa7c1d825f6771a827771ad0ef5
pilar <pilarruete@gmail.com>
2019-02-05 18:52:07 +0100

    Comentario en ejemplo

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 def main():
 	print 'Hola mundo!'
+	//Vamos con operaciones básicas por si alguien quiere crear un branch para esa operción, como si su branch fuera trabajar con esa operación
 def subtract(x,y):
 	return x-y
 def multiply(x,y):
