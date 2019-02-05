743e5e0084694117b46bafcffb14621d00cd850d
pilar <pilarruete@gmail.com>
2019-02-05 11:59:19 +0100

    Modifico desde la rama

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 def main():
 	print 'Hola mundo!'
 	print 'Estoy modificando el repositorio Ejemplo que me he clonado para practicar con git. Ruego disculpéis las molestias y espero no fastidiarla.'
+	print 'Ahora me he creado un branch llamado pilarBranch y  hago cambios desde este branch'
