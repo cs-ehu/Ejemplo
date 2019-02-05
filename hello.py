f21ca775eb1ef0f0d8465e9f117bd4f6acc71ecf
pilar <pilarruete@gmail.com>
2019-02-05 18:07:38 +0100

    Añadiendo operaciones básicas

--- hello.py
+++ hello.py
@@ -2,3 +2,9 @@ def main():
 	print 'Hola mundo!'
 	print 'Estoy modificando el repositorio Ejemplo que me he clonado para practicar con git. Ruego disculpéis las molestias y espero no fastidiarla.'
 	print 'Ahora me he creado un branch llamado pilarBranch y  hago cambios desde este branch'
+def subtract(x,y):
+	return x-y
+def multiply(x,y):
+	return x*y
+def divide(x,y):
+	return x/y
